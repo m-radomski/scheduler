8f4e15727a3345f4b329c55d4cb0379c68bd2959
mateusz <mateusz@asus-proxy>
2020-07-21 13:01:34 +0200

    Moved to modular interface

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/m-radomski/scheduler
+
+go 1.14
+
+require (
+	github.com/gdamore/tcell v1.3.0
+	github.com/jlaffaye/ftp v0.0.0-20200720194710-13949d38913e
+	github.com/rivo/tview v0.0.0-20200712113419-c65badfc3d92
+)
---
+++ src/fetching.go
@@ -0,0 +1,103 @@
+package scheduler
+
+import (
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/jlaffaye/ftp"
+)
+
+var dbPath string = CreateDatabasePath()
+
+func CreateDatabasePath() string {
+	path, exists := os.LookupEnv("XDG_DATA_HOME")
+	if exists {
+		return path + "/scheduler/schedule.json"
+	} else {
+		home := os.Getenv("HOME")
+
+		return home + "/.schedule.json"
+	}
+}
+
+func ReadJson() []Stop {
+	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
+		fmt.Println("Missing database file, fetching it from set FTP server")
+		DatabaseFromFTP(ReadFTPCred("my.cred"))
+	}
+
+	b, err := ioutil.ReadFile(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	var stops []Stop
+	json.Unmarshal(b, &stops)
+	return stops
+}
+
+func FetchFTP(host, username, password string) (b *ftp.Response) {
+	c, err := ftp.Dial(host + ":21", ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.Login(username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := c.Retr("schedule.json.gz")
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	if err := c.Quit(); err != nil {
+		panic(err)
+	}
+
+	return
+}
+
+func DatabaseFromFTP(host, username, password string) {
+	r := FetchFTP(host, username, password)
+
+	// The response is gzip compressed
+	reader, err := gzip.NewReader(r)
+	p, err := ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.OpenFile(dbPath, os.O_RDWR | os.O_CREATE, 0755)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	f.Write(p)
+}
+
+func ReadFTPCred(path string) (host, user, pass string) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	parts := strings.Split(string(b), ";")
+	if len(parts) != 3 {
+		panic("Garbage in ftp credentials")
+	}
+
+	host = parts[0]
+	user = parts[1]
+	pass = parts[2]
+
+	return
+}
